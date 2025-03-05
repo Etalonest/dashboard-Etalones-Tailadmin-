f259f67985fb0fb88eec302ea4c55a33df2a6747
Your Name <your_email@example.com>
2025-03-05 11:35:37 +0200

    сгрупировал партнёров

--- src/components/Tables/TablePartner.tsx
+++ src/components/Tables/TablePartner.tsx
@@ -1,196 +1,20 @@
-// 'use client'
-// import { Eye, UserCog, UserRoundPlus } from "lucide-react";
-// import { Partner } from '@/src/types/partner';  
-// import { useState, useEffect } from 'react';
-// import SidebarRight from '../SidebarRight';
-// import { usePartners } from '@/src/context/PartnerContext';
-// import { useSession } from 'next-auth/react';
-// const TablePartner = () => {
-//   const { data: session } = useSession();
-//   const managerId = session?.managerId ?? 'defaultManagerId';
-//   const { partners, } = usePartners();
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [filteredPartners, setFilteredPartners] = useState<Partner[]>([]);
-//   const partnersPerPage = 10;
-
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
-//     const [formType, setFormType] = useState<"addPartner" | "editPartner" | "viewPartner" | null>(null);
-//     const [selectedPartner, setSelectedPartner] = useState<Partner | null>(null);
-//     const currentPartners = filteredPartners.slice(
-//     (currentPage - 1) * partnersPerPage,
-//     currentPage * partnersPerPage
-//   );
-
-  
-//   // useEffect(() => {
-//   //   if (managerId && partners.length === 0) {
-//   //     loadPartners(managerId);  
-//   //   }
-//   // }, [managerId, loadPartners, partners.length]);
-
-//   useEffect(() => {
-//     if (partners && Array.isArray(partners)) {
-//       const filtered = partners.filter((partner) => {
-//         const lowerCaseSearch = searchQuery.toLowerCase();
-    
-//         // Убедитесь, что partner имеет name и phone
-//         return (
-//           (partner.name && partner.name.toLowerCase().includes(lowerCaseSearch)) ||
-//           (partner.phone && partner.phone.toLowerCase().includes(lowerCaseSearch))
-//         );
-//       });
-//       setFilteredPartners(filtered);
-//     }
-//   }, [searchQuery, partners]);
-
-
-  
-
-
-//   const handlePageChange = (page: number) => {
-//     setCurrentPage(page);
-//   };
-
-//   // Обработчик для изменения поиска
-//   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setSearchQuery(event.target.value);
-//   };
-
-//     const toggleSidebar = (type: "addPartner" | "editPartner" | "viewPartner", partner?: Partner) => {
-//       setFormType(type);             
-//       setSelectedPartner(partner || null); 
-//       setSidebarOpen(prevState => !prevState);  
-//     };
-    
-//   return (
-//     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
-//       <div className='flex justify-between items-center'>
-//       <div className='flex items-center gap-3 mb-6 '>
-
-//         <h4 className="text-xl font-semibold text-black dark:text-white">
-//           Мои партнёры
-//         </h4>
-//         <UserRoundPlus color='green' onClick={() => toggleSidebar("addPartner")} className='cursor-pointer' />
-//         </div>
-//         <SidebarRight 
-//         sidebarROpen={sidebarOpen} 
-//         setSidebarROpen={setSidebarOpen} 
-//         formType={formType} 
-//         selectedPartner={selectedPartner} 
-//         />
-//         <input
-//           type="text"
-//           value={searchQuery}
-//           onChange={handleSearchChange}
-//           placeholder="Поиск по имени, телефону или услуге"
-//           className="mb-4 px-4 py-2  border border-gray-300 rounded-md dark:bg-gray-800 dark:text-white"
-//         />
-//       </div>
-
-//       <div className="flex flex-col">
-//         <div className="grid grid-cols-4 rounded-sm bg-gray-2">
-//           <div className="p-2.5 xl:p-5">
-//             <h5 className="text-sm font-medium uppercase xsm:text-base">
-//               Имя
-//             </h5>
-//           </div>
-          
-//           <div className="p-2.5 text-start xl:p-5">
-//             <h5 className="text-sm font-medium uppercase xsm:text-base">
-//               Документы
-//             </h5>
-//           </div>
-//           <div className="hidden p-2.5 text-center sm:block xl:p-5">
-//             <h5 className="text-sm font-medium uppercase xsm:text-base">
-//               Добавлен/Обновлён
-//             </h5>
-//           </div>
-//           <div className="hidden p-2.5 text-end sm:block xl:p-5">
-//             <h5 className="text-sm font-medium uppercase xsm:text-base">
-//               Телефон
-//             </h5>
-//           </div>
-//         </div>
-
-//         {managerId && currentPartners.map((partner, index) => (
-//           <div
-//             className="grid grid-cols-4" 
-//             key={index}
-//           >
-//             <div className="flex items-center gap-3 p-2.5 xl:p-5">
-//               <div className="flex-shrink-0">
-//                 <Eye />
-//                 <UserCog 
-//                 onClick={() => toggleSidebar("editPartner", partner)}
-//                 />
-//               </div>
-//               <div>
-//               <p className="hidden text-black dark:text-white sm:block">{partner.name}</p>
-// <span>{partner.companyName}</span>
-//               </div>
-//             </div>
-
-           
-
-//             <div className="flex items-center justify-start p-2.5 xl:p-5">
-//               <div className="text-meta-3">
-//                 {partner.documents.map((document,index) => (
-//                   <div key={index}>{document.docType}</div>
-//                 ))}
-//               </div>
-//             </div>
-
-//             <div className="hidden text-sm items-center justify-center p-2.5 sm:flex xl:p-5">
-//               <p className="text-black dark:text-white">
-//                 {`${new Date(partner.createdAt).toLocaleDateString('ru-RU', {
-//                   day: '2-digit', month: 'long', year: '2-digit'
-//                 })} / ${new Date(partner.updatedAt).toLocaleDateString('ru-RU', {
-//                   day: '2-digit', month: 'long', year: '2-digit'
-//                 })}`}
-//               </p>
-//             </div>
-
-//             <div className="hidden items-center justify-end p-2.5 sm:flex xl:p-5">
-//               <p className="text-meta-5">{partner.phone}</p>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-
-//       {/* Пагинация */}
-//       {managerId && filteredPartners.length > partnersPerPage && (
-//         <div className="flex justify-center mt-6">
-//           <button
-//             onClick={() => handlePageChange(currentPage - 1)}
-//             disabled={currentPage === 1}
-//             className="px-4 py-2 text-sm font-semibold bg-blue-500 text-white rounded-md"
-//           >
-//             Назад
-//           </button>
-//           <span className="mx-4 text-sm font-medium text-black dark:text-white">
-//             Страница {currentPage} из {Math.ceil(filteredPartners.length / partnersPerPage)}
-//           </span>
-//           <button
-//             onClick={() => handlePageChange(currentPage + 1)}
-//             disabled={currentPage === Math.ceil(filteredPartners.length / partnersPerPage)}
-//             className="px-4 py-2 text-sm font-semibold bg-blue-500 text-white rounded-md"
-//           >
-//             Вперёд
-//           </button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default TablePartner;
 import { Eye, UserCog, UserRoundPlus } from "lucide-react";
-import { Partner } from '@/src/types/partner';  
+import { Partner } from '@/src/types/partner';
 import { useState, useEffect } from 'react';
 import SidebarRight from '../SidebarRight';
 import { usePartners } from '@/src/context/PartnerContext';
 import { useSession } from 'next-auth/react';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button";
+import React from "react";
+
 
 const TablePartner = () => {
   const { data: session } = useSession();
@@ -203,22 +27,15 @@ const TablePartner = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [formType, setFormType] = useState<"addPartner" | "editPartner" | "viewPartner" | null>(null);
   const [selectedPartner, setSelectedPartner] = useState<Partner | null>(null);
-
-  // Группируем партнёров по статусу
+  const [selectedGroups, setSelectedGroups] = useState<string[]>([]); // состояние для выбранных групп
   const groupedPartners = filteredPartners.reduce((acc, partner) => {
-    const status = partner.status || "Без статуса"; // Используем "Без статуса", если статус не указан
+    const status = partner.status || "Без статуса";
     if (!acc[status]) {
       acc[status] = [];
     }
     acc[status].push(partner);
     return acc;
   }, {} as Record<string, Partner[]>);
-
-  // Пагинированный список партнёров
-  // const currentPartners = Object.values(groupedPartners).flat().slice(
-  //   (currentPage - 1) * partnersPerPage,
-  //   currentPage * partnersPerPage
-  // );
 
   useEffect(() => {
     if (partners && Array.isArray(partners)) {
@@ -233,6 +50,15 @@ const TablePartner = () => {
     }
   }, [searchQuery, partners]);
 
+  // Обновляем фильтрацию на основе выбранных групп
+  const handleGroupChange = (group: string, checked: boolean) => {
+    if (checked) {
+      setSelectedGroups((prev) => [...prev, group]);
+    } else {
+      setSelectedGroups((prev) => prev.filter((g) => g !== group));
+    }
+  };
+
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
@@ -242,10 +68,18 @@ const TablePartner = () => {
   };
 
   const toggleSidebar = (type: "addPartner" | "editPartner" | "viewPartner", partner?: Partner) => {
-    setFormType(type);             
-    setSelectedPartner(partner || null); 
-    setSidebarOpen(prevState => !prevState);  
+    setFormType(type);
+    setSelectedPartner(partner || null);
+    setSidebarOpen((prevState) => !prevState);
   };
+
+  // Фильтрация партнёров по выбранным группам
+  const filteredByGroups = Object.keys(groupedPartners)
+    .filter((group) => selectedGroups.includes(group))
+    .reduce((acc, group) => {
+      acc[group] = groupedPartners[group];
+      return acc;
+    }, {} as Record<string, Partner[]>);
 
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
@@ -256,12 +90,31 @@ const TablePartner = () => {
           </h4>
           <UserRoundPlus color="green" onClick={() => toggleSidebar("addPartner")} className="cursor-pointer" />
         </div>
-        <SidebarRight 
-          sidebarROpen={sidebarOpen} 
-          setSidebarROpen={setSidebarOpen} 
-          formType={formType} 
-          selectedPartner={selectedPartner} 
+        <SidebarRight
+          sidebarROpen={sidebarOpen}
+          setSidebarROpen={setSidebarOpen}
+          formType={formType}
+          selectedPartner={selectedPartner}
         />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Групировать по статусам</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel>Статус</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            {Object.keys(groupedPartners).map((status, index) => (
+              <DropdownMenuCheckboxItem
+                key={index}
+                checked={selectedGroups.includes(status)}
+                onCheckedChange={(checked: boolean) => handleGroupChange(status, checked as boolean)}
+              >
+                {status}
+              </DropdownMenuCheckboxItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        
         <input
           type="text"
           value={searchQuery}
@@ -272,7 +125,7 @@ const TablePartner = () => {
       </div>
 
       <div className="flex flex-col">
-        {Object.keys(groupedPartners).map((status, index) => (
+        {Object.keys(filteredByGroups).map((status, index) => (
           <div key={index}>
             <h5 className="text-lg font-medium text-black dark:text-white mt-6">
               {status}
@@ -285,7 +138,7 @@ const TablePartner = () => {
               <div className="hidden p-2.5 text-end sm:block xl:p-5">Телефон</div>
             </div>
 
-            {groupedPartners[status].map((partner, partnerIndex) => (
+            {filteredByGroups[status].map((partner, partnerIndex) => (
               <div className="grid grid-cols-4" key={partnerIndex}>
                 <div className="flex items-center gap-3 p-2.5 xl:p-5">
                   <div className="flex-shrink-0">
--- src/components/Vacancy/Vacancy.tsx
+++ src/components/Vacancy/Vacancy.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Eye, Plus, Settings, UserRoundPlus } from "lucide-react";
+import { Eye, Plus, Settings } from "lucide-react";
 import { Partner } from '@/src/types/partner';
 import { useState, useEffect, useCallback, } from 'react';
 import SidebarRight from '@/src/components/SidebarRight';
@@ -8,6 +8,7 @@ import { useSession } from 'next-auth/react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { ProfessionPartner } from "@/src/types/professionParnter";
 import { VacancyType } from "@/src/types/vacancy";
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 const Vacancy = () => {
 
   const { data: session } = useSession();
@@ -23,10 +24,20 @@ const Vacancy = () => {
   const [selectedPartner, setSelectedPartner] = useState<Partner | null>(null);
   const [selectedProfession, setSelectedProfession] = useState<ProfessionPartner | null>(null);
   const [selectedVacancy, setSelectedVacancy] = useState<VacancyType | null>(null);
+  const [selectedGroups, setSelectedGroups] = useState<string[]>([]); // для выбранных групп
+
   const currentPartners = filteredPartners.slice(
     (currentPage - 1) * partnersPerPage,
     currentPage * partnersPerPage
   );
+  const groupedPartners = filteredPartners.reduce((acc, partner) => {
+    const status = partner.status || "Без статуса";
+    if (!acc[status]) {
+      acc[status] = [];
+    }
+    acc[status].push(partner);
+    return acc;
+  }, {} as Record<string, Partner[]>);
   const fetchVacancy = async (vacancyId: string) => {
     try {
       const response = await fetch(`/api/vacancy/${vacancyId}`);
@@ -121,7 +132,13 @@ const Vacancy = () => {
     toggleSidebar("viewVacancy", partner, profession, vacancy);
   };
 
-
+  const handleGroupChange = (group: string, checked: boolean) => {
+    if (checked) {
+      setSelectedGroups((prev) => [...prev, group]);
+    } else {
+      setSelectedGroups((prev) => prev.filter((g) => g !== group));
+    }
+  };
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className='flex justify-between items-center'>
@@ -139,6 +156,24 @@ const Vacancy = () => {
           selectedProfession={selectedProfession}
           selectedVacancy={selectedVacancy}
         />
+         <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button className="px-4 py-2 border rounded-md">Группировать по статусу</button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Выберите статус</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            {Object.keys(groupedPartners).map((status) => (
+              <DropdownMenuCheckboxItem
+                key={status}
+                checked={selectedGroups.includes(status)}
+                onCheckedChange={(checked: boolean) => handleGroupChange(status, checked)}
+              >
+                {status}
+              </DropdownMenuCheckboxItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
         <input
           type="text"
           value={searchQuery}
@@ -150,8 +185,8 @@ const Vacancy = () => {
 
       <Table>
         <TableHeader>
-          <TableRow>
-            <TableHead >Заказчик</TableHead>
+          <TableRow className="grid grid-cols-5 gap-2">
+            <TableHead>Заказчик</TableHead>
             <TableHead>Свободные места</TableHead>
             <TableHead className="text-right">На собеседовании</TableHead>
             <TableHead className="text-right">Документы</TableHead>
@@ -159,11 +194,13 @@ const Vacancy = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {managerId && currentPartners.map((partner, index) => (
-            <TableRow
+        {Object.keys(groupedPartners).filter(status => selectedGroups.includes(status)).map((status) => (
+            <div
               // className={`grid grid-cols-3 sm:grid-cols-5 ${'border-b border-stroke dark:border-strokedark'}`}
-              key={index}
+              key={status}
             >
+                            {groupedPartners[status].map((partner, index) => (
+                              <TableRow key={index} className="grid grid-cols-5 gap-2">
               <TableCell>
                 <div className="flex ">
                   <p className="hidden text-black dark:text-white sm:block">{partner.companyName || 'Не указано'}</p>
@@ -215,8 +252,9 @@ const Vacancy = () => {
 
                 </div>
               </TableCell>
-
-            </TableRow>
+              </TableRow>
+                            ))}
+            </div>
           ))}
         </TableBody>
       </Table>
