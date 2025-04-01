ae3b31a77f3bbbb96d033893debbd5de3b4b1c9f
Your Name <your_email@example.com>
2025-04-01 10:59:02 +0300

    исправил добавление партнёра

--- src/app/partner/add/page.tsx
+++ src/app/partner/add/page.tsx
@@ -1,9 +1,10 @@
 import AddpartnerForm from "@/src/components/forms/FormPartner/AddpartnerForm";
+import { ProfessionProvider } from "@/src/context/ProfessionContext";
 
 export default function Page() {
     return (
-        <div>
+        <ProfessionProvider>
             <AddpartnerForm/>
-        </div>
+        </ProfessionProvider>
     )
 }
